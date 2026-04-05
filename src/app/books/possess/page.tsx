b0a9af2b6784a1dad4540096ba3cabbdcc094662
maruKoba55 <hdy.koba.gh55@gmail.com>
2026-04-05 16:06:39 +0900

    batch commit

--- src/app/books/possess/page.tsx
+++ src/app/books/possess/page.tsx
@@ -2,6 +2,9 @@
 import { useState, useRef } from 'react';
 import InsertPossess from '@/app/books/possess/insert_possess';
 
+//vercelによるdeploy時の <Error occurred prerendering page>避け
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div>
